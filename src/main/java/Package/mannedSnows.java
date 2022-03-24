405722beeca15af97792080f0d7dac67831573d9
ClaudiaZorrilla2343 <claudiaa2343@gmail.com>
2022-03-24 11:00:53 -0500

    Terminación de las clases

--- src/main/java/Package/mannedSnows.java
+++ src/main/java/Package/mannedSnows.java
@@ -1,8 +1,11 @@
 package Package;
+
+
 //tripuladas
 public class mannedSnows extends Spaceships {
 
     private int passengers;//pasajeros
+    private String porposeShip;//finalidadNave
 
     public mannedSnows(String name, String motor, String mission, int passengers) {
         super(name, motor, mission);
@@ -13,5 +16,18 @@ public class mannedSnows extends Spaceships {
         System.out.println("Mandar seres humanos para tareas de reparación, matenimiento e investigación");
     }
 
-    
+    public String getPorposeShip() {
+        return porposeShip;
+    }
+
+    public void setPorposeShip(String porposeShip) {
+
+        String porpuse [] = {"Mision Lunar","Experimentación y estudio del comportamiento humano en condiciones ingrávidas " +
+                "y en el exterior de la cápsula", "Mantenimiento de satélites, probar acoplamientos con otras naves y equipos electrónicos"};
+
+        for(int i = 0; i < porpuse.length; i++){
+            System.out.println("La misión número "+i+" es una "+porpuse[i]);
+            this.porposeShip = porpuse[i];
+        }
+    }
 }
